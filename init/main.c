1672b54bb53c9c82d5ba68cc38a335cf5d5cc2f5
Bruce Ashfield <bruce.ashfield@windriver.com>
2010-04-08 23:44:21 -0700

    check console device file on fs when booting
    If a root filesystem is generated as non-root, one of
    the tell tale signs is /dev/console not being a character
    file. To save a whole class of questions, let's just test
    for the condition and let the user know.
    
    Signed-off-by: Richard Laroque <rlarocqu@windriver.com>
    Signed-off-by: Bruce Ashfield <bruce.ashfield@windriver.com>

--- init/main.c
+++ init/main.c
@@ -971,6 +971,7 @@ static int __ref kernel_init(void *unused)
 
 static noinline void __init kernel_init_freeable(void)
 {
+	struct stat console_stat;
 	/*
 	 * Wait until kthreadd is all set-up.
 	 */
@@ -999,6 +1000,12 @@ static noinline void __init kernel_init_freeable(void)
 	sched_init_smp();
 
 	do_basic_setup();
+
+	/* Use /dev/console to infer if the rootfs is setup properly */
+	if (sys_newlstat((char __user *) "/dev/console", (struct stat __user *) &console_stat)
+			|| !S_ISCHR(console_stat.st_mode)) {
+		panic("/dev/console is missing or not a character device!\nPlease ensure your rootfs is properly configured\n");
+	}
 
 	/* Open the /dev/console on the rootfs, this should never fail */
 	if (sys_open((const char __user *) "/dev/console", O_RDWR, 0) < 0)
