c775482cb53e62e7bc0ac71f494ae1b3dac7ecae
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2010-07-03 15:30:57 +0200

    cryptodev: removed

--- drivers/staging/crypto/cryptodev/cryptodev.h
+++ drivers/staging/crypto/cryptodev/cryptodev.h
@@ -98,7 +98,6 @@ struct session_op {
 /* struct crypt_op flags */
 
 #define COP_FLAG_NONE		(0 << 0) /* totally no flag */
-#define COP_FLAG_ZCOPY		(1 << 0) /* operate directly on the userspace buffer */
 
 /* Stuff for bignum arithmetic and public key
  * cryptography - not supported yet by linux
