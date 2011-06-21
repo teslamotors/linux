c7fdd890802ca2332b0a8370d636991c1278db7e
Peter Zijlstra <peterz@infradead.org>
2011-06-21 11:24:35 +0200

    mm-page-alloc-use-list-last-entry.patch
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -702,7 +702,7 @@ static void free_pcppages_bulk(struct zone *zone, int count,
 		do {
 			int mt;	/* migratetype of the to-be-freed page */
 
-			page = list_entry(list->prev, struct page, lru);
+			page = list_last_entry(list, struct page, lru);
 			/* must delete as __free_one_page list manipulates */
 			list_del(&page->lru);
 			mt = get_freepage_migratetype(page);
