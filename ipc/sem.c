36e0b4a2296737ea6bf6339164203f9dab41a1ce
Peter Zijlstra <peterz@infradead.org>
2011-09-14 11:57:04 +0200

    ipc/sem: Rework semaphore wakeups
    Current sysv sems have a weird ass wakeup scheme that involves keeping
    preemption disabled over a potential O(n^2) loop and busy waiting on
    that on other CPUs.
    
    Kill this and simply wake the task directly from under the sem_lock.
    
    This was discovered by a migrate_disable() debug feature that
    disallows:
    
      spin_lock();
      preempt_disable();
      spin_unlock()
      preempt_enable();
    
    Cc: Manfred Spraul <manfred@colorfullife.com>
    Suggested-by: Thomas Gleixner <tglx@linutronix.de>
    Reported-by: Mike Galbraith <efault@gmx.de>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Manfred Spraul <manfred@colorfullife.com>
    Link: http://lkml.kernel.org/r/1315994224.5040.1.camel@twins
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- ipc/sem.c
+++ ipc/sem.c
@@ -673,6 +673,13 @@ undo:
 static void wake_up_sem_queue_prepare(struct list_head *pt,
 				struct sem_queue *q, int error)
 {
+#ifdef CONFIG_PREEMPT_RT_BASE
+	struct task_struct *p = q->sleeper;
+	get_task_struct(p);
+	q->status = error;
+	wake_up_process(p);
+	put_task_struct(p);
+#else
 	if (list_empty(pt)) {
 		/*
 		 * Hold preempt off so that we don't get preempted and have the
@@ -684,6 +691,7 @@ static void wake_up_sem_queue_prepare(struct list_head *pt,
 	q->pid = error;
 
 	list_add_tail(&q->list, pt);
+#endif
 }
 
 /**
@@ -697,6 +705,7 @@ static void wake_up_sem_queue_prepare(struct list_head *pt,
  */
 static void wake_up_sem_queue_do(struct list_head *pt)
 {
+#ifndef CONFIG_PREEMPT_RT_BASE
 	struct sem_queue *q, *t;
 	int did_something;
 
@@ -709,6 +718,7 @@ static void wake_up_sem_queue_do(struct list_head *pt)
 	}
 	if (did_something)
 		preempt_enable();
+#endif
 }
 
 static void unlink_queue(struct sem_array *sma, struct sem_queue *q)
