25ca8ef83d0ecc416a984d2ec2b987bf83bff005
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2013-01-14 22:07:44 +0100

    cryptodev: Use

--- drivers/staging/crypto/cryptodev/extras/eng_cryptodev.c
+++ drivers/staging/crypto/cryptodev/extras/eng_cryptodev.c
@@ -714,8 +714,10 @@ static int cryptodev_digest_update(EVP_MD_CTX *ctx, const void *data,
 		return (1);
 	}
 
-	if (!(ctx->flags & EVP_MD_CTX_FLAG_ONESHOT)) {
-		/* if application doesn't support one buffer */
+        /* If an application wants to compute the hash in one big chunk (one-shot mode)
+         * then tack the data onto the end of the buffer (and grow as necessary).
+         */
+	if ( ctx->flags & EVP_MD_CTX_FLAG_ONESHOT ) {
 		state->mac_data = OPENSSL_realloc(state->mac_data, state->mac_len + count);
 
 		if (!state->mac_data) {
@@ -732,7 +734,9 @@ static int cryptodev_digest_update(EVP_MD_CTX *ctx, const void *data,
 	memset(&cryp, 0, sizeof(cryp));
 
 	cryp.ses = sess->ses;
-	cryp.flags = 0;
+#ifdef COP_FLAG_UPDATE
+	cryp.flags = COP_FLAG_UPDATE;
+#endif
 	cryp.len = count;
 	cryp.src = (void*) data;
 	cryp.dst = NULL;
@@ -756,24 +760,19 @@ static int cryptodev_digest_final(EVP_MD_CTX *ctx, unsigned char *md)
 		return(0);
 	}
 
-	if (! (ctx->flags & EVP_MD_CTX_FLAG_ONESHOT) ) {
-		/* if application doesn't support one buffer */
-		memset(&cryp, 0, sizeof(cryp));
-		cryp.ses = sess->ses;
-		cryp.flags = 0;
-		cryp.len = state->mac_len;
-		cryp.src = state->mac_data;
-		cryp.dst = NULL;
-		cryp.mac = (void*)md;
-		if (ioctl(state->d_fd, CIOCCRYPT, &cryp) < 0) {
-			printf("cryptodev_digest_final: digest failed\n");
-			return (0);
-		}
-
-		return 1;
+	memset(&cryp, 0, sizeof(cryp));
+	cryp.ses = sess->ses;
+#ifdef COP_FLAG_FINAL
+	cryp.flags = COP_FLAG_FINAL;
+#endif
+	cryp.len = state->mac_len;
+	cryp.src = state->mac_data;
+	cryp.dst = NULL;
+	cryp.mac = (void*)md;
+	if (ioctl(state->d_fd, CIOCCRYPT, &cryp) < 0) {
+		printf("cryptodev_digest_final: digest failed\n");
+		return (0);
 	}
-
-	memcpy(md, state->digest_res, ctx->digest->md_size);
 
 	return 1;
 }
