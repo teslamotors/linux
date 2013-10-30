4e11a2a1c43cdb7e62784cb12f2c0582ff0b07f6
Sakari Ailus <sakari.ailus@linux.intel.com>
2013-10-30 22:30:03 +0200

    v4l: Add frame end event
    Add an event to signal frame end. This is not necessarily the same timestamp
    as the video buffer done timestamp, and can be also subscribed by other
    processes than the one controlling streaming and buffer (de)queueing.
    
    Also make all event type constants appear as constants in documentation and
    move frame sync event struct documentation after all control event
    documentation.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Kishore Bodke <kishore.k.bodke@intel.com>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -1937,6 +1937,7 @@ struct v4l2_streamparm {
 #define V4L2_EVENT_FRAME_SYNC			4
 #define V4L2_EVENT_SOURCE_CHANGE		5
 #define V4L2_EVENT_MOTION_DET			6
+#define V4L2_EVENT_FRAME_END			7
 #define V4L2_EVENT_PRIVATE_START		0x08000000
 
 /* Payload for V4L2_EVENT_VSYNC */
@@ -1964,6 +1965,7 @@ struct v4l2_event_ctrl {
 	__s32 default_value;
 };
 
+/* V4L2_EVENT_FRAME_SYNC or V4L2_EVENT_FRAME_END */
 struct v4l2_event_frame_sync {
 	__u32 frame_sequence;
 };
