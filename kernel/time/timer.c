2300b85e986872c9da22967c6ffe945229c304b7
Thomas Gleixner <tglx@linutronix.de>
2013-11-07 12:21:11 +0100

    timers: do not raise softirq unconditionally
    Mike,
    
    On Thu, 7 Nov 2013, Mike Galbraith wrote:
    
    > On Thu, 2013-11-07 at 04:26 +0100, Mike Galbraith wrote:
    > > On Wed, 2013-11-06 at 18:49 +0100, Thomas Gleixner wrote:
    >
    > > > I bet you are trying to work around some of the side effects of the
    > > > occasional tick which is still necessary despite of full nohz, right?
    > >
    > > Nope, I wanted to check out cost of nohz_full for rt, and found that it
    > > doesn't work at all instead, looked, and found that the sole running
    > > task has just awakened ksoftirqd when it wants to shut the tick down, so
    > > that shutdown never happens.
    >
    > Like so in virgin 3.10-rt.  Box is x3550 M3 booted nowatchdog
    > rcu_nocbs=1-3 nohz_full=1-3, and CPUs1-3 are completely isolated via
    > cpusets as well.
    
    well, that very same problem is in mainline if you add "threadirqs" to
    the command line. But we can be smart about this. The untested patch
    below should address that issue. If that works on mainline we can
    adapt it for RT (needs a trylock(&base->lock) there).
    
    Though it's not a full solution. It needs some thought versus the
    softirq code of timers. Assume we have only one timer queued 1000
    ticks into the future. So this change will cause the timer softirq not
    to be called until that timer expires and then the timer softirq is
    going to do 1000 loops until it catches up with jiffies. That's
    anything but pretty ...
    
    What worries me more is this one:
    
      pert-5229  [003] d..h1..   684.482618: softirq_raise: vec=9 [action=RCU]
    
    The CPU has no callbacks as you shoved them over to cpu 0, so why is
    the RCU softirq raised?
    
    Thanks,
    
    	tglx
    ------------------
    Message-id: <alpine.DEB.2.02.1311071158350.23353@ionos.tec.linutronix.de>
    |CONFIG_NO_HZ_FULL + CONFIG_PREEMPT_RT_FULL = nogo
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -1464,8 +1464,6 @@ static void run_timer_softirq(struct softirq_action *h)
 {
 	struct tvec_base *base = __this_cpu_read(tvec_bases);
 
-	hrtimer_run_pending();
-
 	if (time_after_eq(jiffies, base->timer_jiffies))
 		__run_timers(base);
 }
@@ -1475,8 +1473,32 @@ static void run_timer_softirq(struct softirq_action *h)
  */
 void run_local_timers(void)
 {
+	struct tvec_base *base = __this_cpu_read(tvec_bases);
+
 	hrtimer_run_queues();
-	raise_softirq(TIMER_SOFTIRQ);
+	/*
+	 * We can access this lockless as we are in the timer
+	 * interrupt. If there are no timers queued, nothing to do in
+	 * the timer softirq.
+	 */
+#ifdef CONFIG_PREEMPT_RT_FULL
+	if (!spin_do_trylock(&base->lock)) {
+		raise_softirq(TIMER_SOFTIRQ);
+		return;
+	}
+#endif
+	if (!base->active_timers)
+		goto out;
+
+	/* Check whether the next pending timer has expired */
+	if (time_before_eq(base->next_timer, jiffies))
+		raise_softirq(TIMER_SOFTIRQ);
+out:
+#ifdef CONFIG_PREEMPT_RT_FULL
+	rt_spin_unlock_after_trylock_in_irq(&base->lock);
+#endif
+	/* The ; ensures that gcc won't complain in the !RT case */
+	;
 }
 
 #ifdef __ARCH_WANT_SYS_ALARM
