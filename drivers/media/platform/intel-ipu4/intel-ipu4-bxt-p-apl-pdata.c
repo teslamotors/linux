a82f4ee48539d32a35b233a68c8d73479ed890a0
Samu Onkalo <samu.onkalo@intel.com>
2015-11-30 14:15:51 +0200

    intel-ipu4: platformdata: remove ACPI devices
    Remove ACPI supported devices from platform data
    files.
    
    Signed-off-by: Samu Onkalo <samu.onkalo@intel.com>
    Signed-off-by: Kishore Bodke <kishore.k.bodke@intel.com>

--- drivers/media/platform/intel-ipu4/intel-ipu4-bxt-p-apl-pdata.c
+++ drivers/media/platform/intel-ipu4/intel-ipu4-bxt-p-apl-pdata.c
@@ -25,12 +25,6 @@
 #include <linux/regulator/machine.h>
 
 #include <media/intel-ipu4-isys.h>
-#include <media/smiapp.h>
-#include "../../../../include/media/crlmodule.h"
-#include "../../../../include/media/lm3643.h"
-
-#define IMX135_LANES		4
-#define IMX135_I2C_ADDRESS	0x10
 
 #define GPIO_BASE		434
 
@@ -72,45 +66,15 @@ static struct platform_device kilshoncamc = {
 	},
 };
 
-static struct crlmodule_platform_data imx135_pdata = {
-	.xshutdown = GPIO_BASE + 55,
-	.lanes = IMX135_LANES,
-	.ext_clk = 24000000,
-	.op_sys_clock = (uint64_t []){ 1804800000 / IMX135_LANES,
-				       1368000000 / IMX135_LANES,
-				       838400000 / IMX135_LANES,
-				       0 },
-};
-
-static struct intel_ipu4_isys_csi2_config imx135_csi2_cfg = {
-	.nlanes = IMX135_LANES,
-	.port = 4,
-};
-
-static struct intel_ipu4_isys_subdev_info imx135_sd = {
-	.csi2 = &imx135_csi2_cfg,
-	.i2c = {
-		.board_info = {
-			 I2C_BOARD_INFO(CRLMODULE_NAME, IMX135_I2C_ADDRESS),
-			 .platform_data = &imx135_pdata,
-		},
-		.i2c_adapter_id = 2,
-	},
-	.acpiname = "i2c-INT3471:00",
-};
-
 /*
  * Map buttress output sensor clocks to sensors -
- * this should be coming from ACPI
  */
 static struct intel_ipu4_isys_clk_mapping clk_mapping[] = {
-	{ CLKDEV_INIT("i2c-INT3471:00",  NULL, NULL), "OSC_CLK_OUT2" },
 	{ CLKDEV_INIT(NULL, NULL, NULL), NULL }
 };
 
 static struct intel_ipu4_isys_subdev_pdata pdata = {
 	.subdevs = (struct intel_ipu4_isys_subdev_info *[]) {
-		&imx135_sd,
 		NULL,
 	},
 	.clk_map = clk_mapping,
