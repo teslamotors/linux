85e18afc7997aca0570e65c4b05b18d64d589df1
wu xia <xia.wu@intel.com>
2015-12-07 15:37:14 +0200

    media: Add media device request state
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: wu xia <xia.wu@intel.com>

--- drivers/media/media-device.c
+++ drivers/media/media-device.c
@@ -34,6 +34,16 @@
 #include <media/media-entity.h>
 
 #ifdef CONFIG_MEDIA_CONTROLLER
+
+static char *__request_state[] = {
+	"IDLE",
+	"QUEUED",
+};
+
+#define request_state(i)			\
+	((i) < ARRAY_SIZE(__request_state) ? __request_state[i] : "UNKNOWN")
+
+
 struct media_device_fh {
 	struct media_devnode_fh fh;
 	struct list_head requests;
@@ -136,6 +146,7 @@ static int media_device_request_alloc(struct media_device *mdev,
 	req->mdev = mdev;
 	req->id = id;
 	req->filp = filp;
+	req->state = MEDIA_DEVICE_REQUEST_STATE_IDLE;
 	kref_init(&req->kref);
 
 	spin_lock_irqsave(&mdev->req_lock, flags);
