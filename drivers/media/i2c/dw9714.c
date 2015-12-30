33eb06e313cec45f1b9058ecc7b404a7920649c3
Sakari Ailus <sakari.ailus@linux.intel.com>
2015-12-30 13:26:30 +0200

    dw9714: Fix copyright notices
    Update the year and remove "All rights reserved." The patch has been
    produced using this command:
    
    	find -name '*.[ch]'|grep -v 'ipu4_bxt.0'|while read i; do
    		perl -i -pe 's/ ?all rights reserved.$//i;
    			s/Copyright \(c\) 201[3-4]\K[^-]/--2015 /;
    			s/\(c\) 201.--\K201./2015/;' $i
    	done
    
    	perl -e '
    		my %h = (
    			"crlmodule" => [
    				"drivers/media/i2c/crlmodule",
    				"include/uapi/linux/crlmodule.h",
    				"include/media/crlmodule.h" ],
    			"intel-ipu4" => [
    			"drivers/media/pci/intel-ipu4/",
    			"include/media/intel-ipu4-isys.h",
    			"include/uapi/linux/".
    				"intel-ipu4-isys-isa-fw.h",
    			"include/uapi/linux/intel-ipu4-isys.h",
    			"include/uapi/linux/intel-ipu4-psys.h" ],
    			"platform" => [ "drivers/media/platform/" ],
    			"lm3643" => [
    				"drivers/media/i2c/lm3643.c",
    				"include/media/lm3643.h" ],
    			"lc898122" => [
    				"drivers/media/i2c/lc898122/" ],
    			"dw9714" => [
    				"drivers/media/i2c/dw9714.c",
    				"drivers/media/i2c/dw9714.h" ],
    			);
    		foreach my $name (keys %h) {
    			my $ref = $h{$name};
    			system "(echo -n $name; cat message) ".
    				"| git commit ".
    				"-s -F - --no-edit " . (join " ",
    				@$ref) . "\n";
    		}
    	'
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Kishore Bodke <kishore.k.bodke@intel.com>

--- drivers/media/i2c/dw9714.c
+++ drivers/media/i2c/dw9714.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Intel Corporation. All Rights Reserved.
+ * Copyright (c) 2015 Intel Corporation.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version
