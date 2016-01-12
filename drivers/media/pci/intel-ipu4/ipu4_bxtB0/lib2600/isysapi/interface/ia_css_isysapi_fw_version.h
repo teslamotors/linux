35612fa7a737669a0f7cfcb2b050ad8a781cd062
sys_icgvert <icgvert@intel.com>
2016-01-12 13:26:25 -0800

    isys package lib2600 b0 package for scci_master_20160112_0009
    FW : tag scci_master_20160112_0009
    
    Signed-off-by: sys_icgvert <icgvert@intel.com>
    Signed-off-by: Kishore Bodke <kishore.k.bodke@intel.com>

--- drivers/media/pci/intel-ipu4/ipu4_bxtB0/lib2600/isysapi/interface/ia_css_isysapi_fw_version.h
+++ drivers/media/pci/intel-ipu4/ipu4_bxtB0/lib2600/isysapi/interface/ia_css_isysapi_fw_version.h
@@ -16,6 +16,6 @@
 #define __IA_CSS_ISYSAPI_FW_VERSION_H__
 
 /* ISYSAPI FW VERSION is taken from Makefile for FW tests */
-#define BXT_FW_RELEASE_VERSION "scci_master_20160106_2243"
+#define BXT_FW_RELEASE_VERSION "scci_master_20160112_0009"
 
 #endif /* __IA_CSS_ISYSAPI_FW_VERSION_H__ */
