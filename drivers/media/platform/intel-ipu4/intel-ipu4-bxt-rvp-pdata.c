fd79adcd511c36e5056bc8e1030464e2efb894c5
Samu Onkalo <samu.onkalo@intel.com>
2016-02-11 10:24:45 +0200

    platform data: Development time platform data for as3638
    Add platform data for as3638 for development purposes.
    
    Signed-off-by: Soren Friis <soren.friis@intel.com>
    Signed-off-by: Samu Onkalo <samu.onkalo@intel.com>
    Signed-off-by: Kishore Bodke <kishore.k.bodke@intel.com>

--- drivers/media/platform/intel-ipu4/intel-ipu4-bxt-rvp-pdata.c
+++ drivers/media/platform/intel-ipu4/intel-ipu4-bxt-rvp-pdata.c
@@ -20,13 +20,51 @@
 #include <linux/pci.h>
 
 #include <media/intel-ipu4-isys.h>
+#include "../../../../include/media/as3638.h"
 
 #define GPIO_BASE		429
+
+/* #define AS3638_DEVEL */
 
 /*
  * Placeholder for possible bxt_rvp platform data for testing different
  * AOB etc. non bios supported configurations
  */
+
+/*
+ * Development time configuration
+ *
+ * as3638
+ * - i2c: pmic_i2c_2: 0x30
+ * - regulator: v_vsys (always on)
+ * - Enable = Flash_RST_N = GP_CAMERASB00 = North 62
+ * - Torch = Flash_Torch = GP_CAMERASB01 = North 63
+ * - Strobe = Flash_Trig = GP_CAMERASB02 = North 64
+ */
+#ifdef AS3638_DEVEL
+static struct as3638_platform_data as3638_pdata = {
+	.gpio_reset = GPIO_BASE + 62,
+	.gpio_torch = GPIO_BASE + 63,
+	.gpio_strobe = GPIO_BASE + 64,
+	.flash_max_brightness[AS3638_LED1] = AS3638_FLASH_MAX_BRIGHTNESS_LED1,
+	.torch_max_brightness[AS3638_LED1] = AS3638_TORCH_MAX_BRIGHTNESS_LED1,
+	.flash_max_brightness[AS3638_LED2] = AS3638_FLASH_MAX_BRIGHTNESS_LED2,
+	.torch_max_brightness[AS3638_LED2] = AS3638_TORCH_MAX_BRIGHTNESS_LED2,
+	.flash_max_brightness[AS3638_LED3] = AS3638_FLASH_MAX_BRIGHTNESS_LED3,
+	.torch_max_brightness[AS3638_LED3] = AS3638_TORCH_MAX_BRIGHTNESS_LED3,
+};
+
+static struct intel_ipu4_isys_subdev_info as3638_sd = {
+	.i2c = {
+		.board_info = {
+			 I2C_BOARD_INFO(AS3638_NAME,  AS3638_I2C_ADDR),
+			 .platform_data = &as3638_pdata,
+		},
+		.i2c_adapter_id = 2,
+	},
+/*	.acpiname = "i2c-AMS3638:00", */
+};
+#endif
 
 /*
  * Map buttress output sensor clocks to sensors defined here -
@@ -37,6 +75,9 @@ static struct intel_ipu4_isys_clk_mapping clk_mapping[] = {
 
 static struct intel_ipu4_isys_subdev_pdata pdata = {
 	.subdevs = (struct intel_ipu4_isys_subdev_info *[]) {
+#ifdef AS3638_DEVEL
+		&as3638_sd,
+#endif
 		NULL,
 	},
 	.clk_map = clk_mapping,
