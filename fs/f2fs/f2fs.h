ed9c517cdc65cb47f3e788a0f0744031ca0d8458
Yang Shi <yang.shi@linaro.org>
2016-02-26 16:25:25 -0800

    f2fs: Mutex can't be used by down_write_nest_lock()
    fsf2_lock_all() calls down_write_nest_lock() to acquire a rw_sem and check
    a mutex, but down_write_nest_lock() is designed for two rw_sem accoring to the
    comment in include/linux/rwsem.h. And, other than f2fs, it is just called in
    mm/mmap.c with two rwsem.
    
    So, it looks it is used wrongly by f2fs. And, it causes the below compile
    warning on -rt kernel too.
    
    In file included from fs/f2fs/xattr.c:25:0:
    fs/f2fs/f2fs.h: In function 'f2fs_lock_all':
    fs/f2fs/f2fs.h:962:34: warning: passing argument 2 of 'down_write_nest_lock' from
    		       incompatible pointer type [-Wincompatible-pointer-types]
      f2fs_down_write(&sbi->cp_rwsem, &sbi->cp_mutex);
                                      ^
    
    The nest annotation was anyway bogus as nested annotations for lockdep are
    only required if one nests two locks of the same lock class, which is not the
    case here.
    
    Signed-off-by: Yang Shi <yang.shi@linaro.org>
    Cc: cm224.lee@samsung.com
    Cc: chao2.yu@samsung.com
    Cc: linaro-kernel@lists.linaro.org
    Cc: linux-rt-users@vger.kernel.org
    Cc: bigeasy@linutronix.de
    Cc: rostedt@goodmis.org
    Cc: linux-f2fs-devel@lists.sourceforge.net
    Cc: linux-fsdevel@vger.kernel.org
    Cc: jaegeuk@kernel.org
    Link: http://lkml.kernel.org/r/1456532725-4126-1-git-send-email-yang.shi@linaro.org
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -22,7 +22,6 @@
 
 #ifdef CONFIG_F2FS_CHECK_FS
 #define f2fs_bug_on(sbi, condition)	BUG_ON(condition)
-#define f2fs_down_write(x, y)	down_write_nest_lock(x, y)
 #else
 #define f2fs_bug_on(sbi, condition)					\
 	do {								\
@@ -31,7 +30,6 @@
 			sbi->need_fsck = true;				\
 		}							\
 	} while (0)
-#define f2fs_down_write(x, y)	down_write(x)
 #endif
 
 /*
@@ -699,7 +697,7 @@ static inline void f2fs_unlock_op(struct f2fs_sb_info *sbi)
 
 static inline void f2fs_lock_all(struct f2fs_sb_info *sbi)
 {
-	f2fs_down_write(&sbi->cp_rwsem, &sbi->cp_mutex);
+	down_write(&sbi->cp_rwsem);
 }
 
 static inline void f2fs_unlock_all(struct f2fs_sb_info *sbi)
