6b3b7078a485a7ff60e7dce9551f4a95786f514b
Riley Andrews <riandrews@google.com>
2016-05-05 14:42:41 -0700

    trusty: Add trusty logging driver.
    This driver is the consumer side of a ringbuffer of log data
    that the secure operating system dumps prints into. Trusty
    printfs will be dumped into the kernel log after smc calls
    and during panics.
    
    Change-Id: Iadc939b60940330e8fe02a52f3e397da7833c2fa

--- include/linux/trusty/smcall.h
+++ include/linux/trusty/smcall.h
@@ -51,6 +51,7 @@
 #define	SMC_ENTITY_RESERVED		5	/* Reserved for future use */
 #define	SMC_ENTITY_TRUSTED_APP		48	/* Trusted Application calls */
 #define	SMC_ENTITY_TRUSTED_OS		50	/* Trusted OS calls */
+#define	SMC_ENTITY_LOGGING		51	/* Used for secure -> nonsecure logging */
 #define	SMC_ENTITY_SECURE_MONITOR	60	/* Trusted OS calls internal to secure monitor */
 
 /* FC = Fast call, SC = Standard call */
