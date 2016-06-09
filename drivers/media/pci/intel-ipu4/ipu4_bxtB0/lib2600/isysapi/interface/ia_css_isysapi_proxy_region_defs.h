04f2dec3fa8978de22d939aa8bc5ac01c026839d
sys_icgvert <icgvert@intel.com>
2016-06-08 18:45:24 -0700

    isys package lib2600b0 for scci_master_20160608_1322
    Signed-off-by: sys_icgvert <icgvert@intel.com>
    Signed-off-by: Kishore Bodke <kishore.k.bodke@intel.com>

--- drivers/media/pci/intel-ipu4/ipu4_bxtB0/lib2600/isysapi/interface/ia_css_isysapi_proxy_region_defs.h
+++ drivers/media/pci/intel-ipu4/ipu4_bxtB0/lib2600/isysapi/interface/ia_css_isysapi_proxy_region_defs.h
@@ -61,11 +61,13 @@ enum bxt_b0_ia_css_proxy_write_region {
 	BXT_B0_IA_CSS_PROXY_WRITE_REGION_STR2MMIO_MIPI_7_ERROR_FILL_RATE,
 	BXT_B0_IA_CSS_PROXY_WRITE_REGION_STR2MMIO_MIPI_8_ERROR_FILL_RATE,
 	BXT_B0_IA_CSS_PROXY_WRITE_REGION_STR2MMIO_MIPI_9_ERROR_FILL_RATE,
+	BXT_B0_IA_CSS_PROXY_WRITE_REGION_GDA_IRQ_URGENT_THRESHOLD,
+	BXT_B0_IA_CSS_PROXY_WRITE_REGION_GDA_IRQ_CRITICAL_THRESHOLD,
 	N_BXT_B0_IA_CSS_PROXY_WRITE_REGION
 };
 
 struct ia_css_proxy_write_region_description bxt_b0_reg_write_desc[N_BXT_B0_IA_CSS_PROXY_WRITE_REGION] = {
-	/* base_addr, offset */
+	/* base_addr, 										offset */
 	{0x64128,	/*input_system_csi2_logic_s2m_a_stream2mmio_err_mode_dc_ctrl_reg_id*/		4},	/*BXT_B0_IA_CSS_PROXY_WRITE_REGION_STR2MMIO_MIPI_0_ERROR_FILL_RATE*/
 	{0x65128,	/*input_system_csi2_logic_s2m_b_stream2mmio_err_mode_dc_ctrl_reg_id*/		4},	/*BXT_B0_IA_CSS_PROXY_WRITE_REGION_STR2MMIO_MIPI_1_ERROR_FILL_RATE*/
 	{0x66128,	/*input_system_csi2_logic_s2m_c_stream2mmio_err_mode_dc_ctrl_reg_id*/		4},	/*BXT_B0_IA_CSS_PROXY_WRITE_REGION_STR2MMIO_MIPI_2_ERROR_FILL_RATE*/
@@ -75,7 +77,9 @@ struct ia_css_proxy_write_region_description bxt_b0_reg_write_desc[N_BXT_B0_IA_C
 	{0x6D128,	/*input_system_csi2_3ph_logic_s2m_0_stream2mmio_err_mode_dc_ctrl_reg_id*/	4},	/*BXT_B0_IA_CSS_PROXY_WRITE_REGION_STR2MMIO_MIPI_6_ERROR_FILL_RATE*/
 	{0x6D928,	/*input_system_csi2_3ph_logic_s2m_1_stream2mmio_err_mode_dc_ctrl_reg_id*/	4},	/*BXT_B0_IA_CSS_PROXY_WRITE_REGION_STR2MMIO_MIPI_7_ERROR_FILL_RATE*/
 	{0x6E128,	/*input_system_csi2_3ph_logic_s2m_2_stream2mmio_err_mode_dc_ctrl_reg_id*/	4},	/*BXT_B0_IA_CSS_PROXY_WRITE_REGION_STR2MMIO_MIPI_8_ERROR_FILL_RATE*/
-	{0x6E928,	/*input_system_csi2_3ph_logic_s2m_3_stream2mmio_err_mode_dc_ctrl_reg_id*/	4}	/*BXT_B0_IA_CSS_PROXY_WRITE_REGION_STR2MMIO_MIPI_9_ERROR_FILL_RATE*/
+	{0x6E928,	/*input_system_csi2_3ph_logic_s2m_3_stream2mmio_err_mode_dc_ctrl_reg_id*/	4},	/*BXT_B0_IA_CSS_PROXY_WRITE_REGION_STR2MMIO_MIPI_9_ERROR_FILL_RATE*/
+	{0x7800C,	/*input_system_unis_logic_gda_irq_urgent_threshold*/				4},	/*BXT_B0_IA_CSS_PROXY_WRITE_REGION_GDA_IRQ_URGENT_THRESHOLD*/
+	{0x78010,	/*input_system_unis_logic_gda_irq_critical_threshold*/				4}	/*BXT_B0_IA_CSS_PROXY_WRITE_REGION_GDA_IRQ_CRITICAL_THRESHOLD*/
 };
 
 #endif /*defined(BXT_B0_PROXY_INT)*/
