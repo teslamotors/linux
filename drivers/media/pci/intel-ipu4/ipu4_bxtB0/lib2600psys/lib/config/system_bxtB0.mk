eabaffe96a24a88aed7efc88253a412f67e2f056
sys_icgvert <icgvert@intel.com>
2016-06-14 17:51:05 -0700

    psys_spc package lib2600b0 for scci_master_20160614_1333
    Signed-off-by: sys_icgvert <icgvert@intel.com>
    Signed-off-by: Kishore Bodke <kishore.k.bodke@intel.com>

--- drivers/media/pci/intel-ipu4/ipu4_bxtB0/lib2600psys/lib/config/system_bxtB0.mk
+++ drivers/media/pci/intel-ipu4/ipu4_bxtB0/lib2600psys/lib/config/system_bxtB0.mk
@@ -13,6 +13,7 @@ ISP_CELL                        = isp2601
 ISP_CELL_IDENTIFIER             = ISP2601
 HAS_IPFD                        = 1
 HAS_STR2MMIO_IN_ISL_NONSOC_PATH = 0
+HAS_PIXEL_FORMATTER_IN_ISYS	= 1
 DLI_SYSTEM                      = hive_isp_css_2600_system
 USE_REG_DUMP			= 1
 RESOURCE_MANAGER_VERSION        = v1
