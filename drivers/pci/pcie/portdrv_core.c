446a452388d7cfc9a5bde230064a5ff0c11fe5a8
Mika Westerberg <mika.westerberg@linux.intel.com>
2016-06-30 01:03:23 +0000

    PCI: Add runtime PM support for PCIe ports
    Add back runtime PM support for PCIe ports that was removed in
    commit fe9a743a2601 ("PCI/PM: Drop unused runtime PM support code for PCIe
    ports").
    
    First of all we cannot enable it automatically for all ports since there
    has been problems previously as can be seen in [1].  In summary suspended
    PCIe ports were not able to deal with ACPI-based hotplug reliably.  One
    reason why this might happen is the fact that when a PCIe port is powered
    down, config space access to the devices behind the port is not possible.
    If the BIOS hotplug SMI handler assumes the port is always in D0 it will
    not be able to find the hotplugged devices.  To be on the safe side only
    enable runtime PM if the port does not claim to support hotplug.
    
    For PCIe ports not using hotplug, we enable and allow runtime PM
    automatically.  Since 'bridge_d3' can be changed any time we check this in
    driver ->runtime_idle() and ->runtime_suspend() and only allow runtime
    suspend if the flag is still set.  Use autosuspend with default of 100ms
    idle time to prevent the port from repeatedly suspending and resuming on
    continuous configuration space access of devices behind the port.
    
    The actual power transition to D3 and back is handled in the PCI core.
    
    Idea to automatically unblock (allow) runtime PM for PCIe ports came from
    Dave Airlie.
    
    [1] https://bugzilla.kernel.org/show_bug.cgi?id=53811
    
    This includes a fix for lockdep issue reported by Valdis Kletnieks.
    
    Tested-by: Lukas Wunner <lukas@wunner.de>
    Signed-off-by: Lukas Wunner <lukas@wunner.de>
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Yong, Jonathan <jonathan.yong@intel.com>
    Signed-off-by: Bruce Ashfield <bruce.ashfield@windriver.com>

--- drivers/pci/pcie/portdrv_core.c
+++ drivers/pci/pcie/portdrv_core.c
@@ -11,6 +11,7 @@
 #include <linux/kernel.h>
 #include <linux/errno.h>
 #include <linux/pm.h>
+#include <linux/pm_runtime.h>
 #include <linux/string.h>
 #include <linux/slab.h>
 #include <linux/pcieport_if.h>
@@ -349,6 +350,8 @@ static int pcie_device_init(struct pci_dev *pdev, int service, int irq)
 		put_device(device);
 		return retval;
 	}
+
+	pm_runtime_no_callbacks(device);
 
 	return 0;
 }
