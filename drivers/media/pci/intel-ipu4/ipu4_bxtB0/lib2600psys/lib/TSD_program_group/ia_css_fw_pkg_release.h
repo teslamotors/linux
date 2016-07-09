3650998876cf7e5f3832c6461d2051269a84b59a
sys_icgvert <icgvert@intel.com>
2016-07-08 20:05:11 -0700

    isys & psys package lib2600b0 for scci_master_20160708_1500
    Releasing on scci_master_20160707_1810
    
    c190d68 [EXY OPT] Fully enabled EXY kernel in vhdr pregdc PG.
    2016-06-22
    07ad14e [#H1405180620] Fixed malformed haaf 2016-07-06
    6bab8db [#H1405171042] Fixed missing prm 2016-07-06
    70c8805 [#H1405177508 #H1405190308] Updated ref images for
    VPreHQ + vpost 2016-07-06
    9af15a7 [#H1405170866] Updated vpostgdc silver reference files
    2016-07-01
    3b4a502f [isl_ps_hires crop, scale] adding tests for isl_ps crop
    & scale 2016-06-30
    
    Signed-off-by: sys_icgvert <icgvert@intel.com>
    Signed-off-by: Kishore Bodke <kishore.k.bodke@intel.com>

--- drivers/media/pci/intel-ipu4/ipu4_bxtB0/lib2600psys/lib/TSD_program_group/ia_css_fw_pkg_release.h
+++ drivers/media/pci/intel-ipu4/ipu4_bxtB0/lib2600psys/lib/TSD_program_group/ia_css_fw_pkg_release.h
@@ -11,4 +11,4 @@
 * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 * more details.
 */
-#define IA_CSS_FW_PKG_RELEASE  0x20160707 
+#define IA_CSS_FW_PKG_RELEASE  0x20160708 
