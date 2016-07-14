40c0a26d8b3e6c20cf0e1941288b8f54dcd00db5
Ho, Yu Xin <yu.xin.ho@intel.com>
2016-07-14 11:06:18 +0800

    ASoC: Intel: Skylake: Remove dummy codec SSP ports mute unmute switch
    This commit attempts to remove mute unmute control for SSP ports
    with dummy codec. This is due to the fact that mute and unmute command
    is not supported in FW unless there is a gain block. If mute and
    unmute command is sent to FW, unknown IPC message and pipeline
    error will occur.
    
    Signed-off-by: Ho, Yu Xin <yu.xin.ho@intel.com>
    (cherry picked from commit c80aa78cceb961f31d9b19d55e2a1fc9a2b4975a)
    Signed-off-by: SengKai,Tan <seng.kai.tan@intel.com>

--- sound/soc/intel/boards/apli_lhcrb_wm8731.c
+++ sound/soc/intel/boards/apli_lhcrb_wm8731.c
@@ -67,15 +67,11 @@ static struct snd_soc_ops apli_lfcrb_wm8731_ops = {
 };
 
 static const struct snd_kcontrol_new apli_controls[] = {
-	SOC_DAPM_PIN_SWITCH("SSP2 Speaker"),
-	SOC_DAPM_PIN_SWITCH("SSP2 Mic"),
 	SOC_DAPM_PIN_SWITCH("Headphone Jack"),
 	SOC_DAPM_PIN_SWITCH("Mic Jack"),
 };
 
 static const struct snd_soc_dapm_widget apli_widgets[] = {
-	SND_SOC_DAPM_SPK("SSP2 Speaker", NULL),
-	SND_SOC_DAPM_MIC("SSP2 Mic", NULL),
 	SND_SOC_DAPM_SPK("SSP4 Speaker", NULL),
 	SND_SOC_DAPM_MIC("SSP4 Mic", NULL),
 	SND_SOC_DAPM_HP("Headphone Jack", NULL),
