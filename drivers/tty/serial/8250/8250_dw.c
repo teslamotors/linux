d81c312441453caf7f691a65dcea9bf6e4a66075
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2016-09-12 14:40:45 +0300

    serial: 8250_dw: enable runtime PM
    [Taken from https://bitbucket.org/andy-shev/linux/branch/topic/uart/rpm]
    
    Enable runtime PM for all ports unconditionally. Set autosuspend time to
    infinity (-1) to avoid port being shutdown when using, for example, as a
    serial console since there is no means to resume it back.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Wan Ahmad Zainie <wan.ahmad.zainie.wan.mohamad@intel.com>

--- drivers/tty/serial/8250/8250_dw.c
+++ drivers/tty/serial/8250/8250_dw.c
@@ -242,18 +242,6 @@ static int dw8250_handle_irq(struct uart_port *p)
 	return 0;
 }
 
-static void
-dw8250_do_pm(struct uart_port *port, unsigned int state, unsigned int old)
-{
-	if (!state)
-		pm_runtime_get_sync(port->dev);
-
-	serial8250_do_pm(port, state, old);
-
-	if (state)
-		pm_runtime_put_sync_suspend(port->dev);
-}
-
 static void dw8250_set_termios(struct uart_port *p, struct ktermios *termios,
 			       struct ktermios *old)
 {
@@ -434,7 +422,6 @@ static int dw8250_probe(struct platform_device *pdev)
 	p->mapbase	= regs->start;
 	p->irq		= irq;
 	p->handle_irq	= dw8250_handle_irq;
-	p->pm		= dw8250_do_pm;
 	p->type		= PORT_8250;
 	p->flags	= UPF_SHARE_IRQ | UPF_FIXED_PORT;
 	p->dev		= dev;
@@ -562,6 +549,9 @@ static int dw8250_probe(struct platform_device *pdev)
 	}
 
 	platform_set_drvdata(pdev, data);
+
+	pm_runtime_use_autosuspend(dev);
+	pm_runtime_set_autosuspend_delay(dev, -1);
 
 	pm_runtime_set_active(dev);
 	pm_runtime_enable(dev);
