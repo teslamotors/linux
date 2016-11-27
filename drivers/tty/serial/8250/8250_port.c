5c5818604b05e99adbfb98a061803d3fa7f30c19
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2016-11-27 20:21:23 +0200

    serial: 8250_port: Disable DMA operations for kernel console
    [Taken from https://bitbucket.org/andy-shev/linux/branch/topic/uart/rpm]
    
    It will be too tricky and error prone to allow DMA operations on kernel
    console.
    
    Disable any kind of DMA operations in such case.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Wan Ahmad Zainie <wan.ahmad.zainie.wan.mohamad@intel.com>

--- drivers/tty/serial/8250/8250_port.c
+++ drivers/tty/serial/8250/8250_port.c
@@ -2394,7 +2394,10 @@ dont_test_tx_en:
 	 * Request DMA channels for both RX and TX.
 	 */
 	if (up->dma) {
-		retval = serial8250_request_dma(up);
+		if (uart_console(port))
+			retval = -ENXIO;
+		else
+			retval = serial8250_request_dma(up);
 		if (retval) {
 			pr_warn_ratelimited("ttyS%d - failed to request DMA\n",
 					    serial_index(port));
