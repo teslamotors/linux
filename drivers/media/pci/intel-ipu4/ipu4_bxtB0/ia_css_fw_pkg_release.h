ce52dac78fd13cd3cc51bf35a89bc40994ccdcaf
sys_icgvert <icgvert@intel.com>
2017-01-30 17:38:04 -0800

    isys & psys package for scci_stable_iot_20161227_20170126_1840
    Releasing on scci_stable_iot_20161227_20170118_0226
    
    7b40c77 [YUV_CSC] Support for BGR565 for yuv_csc pg 2017-01-24
    
    FW IoT_Stable: tag scci_stable_iot_20161227_20170126_1840
    
    Signed-off-by: sys_icgvert <icgvert@intel.com>
    Signed-off-by: Kishore Bodke <kishore.k.bodke@intel.com>

--- drivers/media/pci/intel-ipu4/ipu4_bxtB0/ia_css_fw_pkg_release.h
+++ drivers/media/pci/intel-ipu4/ipu4_bxtB0/ia_css_fw_pkg_release.h
@@ -11,4 +11,4 @@
 * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
 * more details.
 */
-#define IA_CSS_FW_PKG_RELEASE  0x20170118
+#define IA_CSS_FW_PKG_RELEASE  0x20170126
