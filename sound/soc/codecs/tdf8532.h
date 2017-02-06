fc51bf06ec07b34b2fb58fa95a9b5ac1d40e4759
CraigX Kewley <craigx.kewley@intel.com>
2017-02-06 09:12:57 +0000

    ASoC: TDF8532: Add generic write function
    Allow future expansion of the codec driver by adding generic write function
    and macro. Also update all writes to use this.
    
    Signed-off-by: CraigX Kewley <craigx.kewley@intel.com>
    
    Conflicts:
    	sound/soc/codecs/tdf8532.h

--- sound/soc/codecs/tdf8532.h
+++ sound/soc/codecs/tdf8532.h
@@ -1,18 +1,3 @@
-/*
- * tdf8532.h - Codec driver for NXP Semiconductors
- * Copyright (c) 2017, Intel Corporation.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- */
-
-
 #ifndef __TDF8532_H_
 #define __TDF8532_H_
 
@@ -42,49 +27,6 @@
 
 #define SET_CHNL_MUTE 0x42
 #define SET_CHNL_UNMUTE 0x43
-
-struct header_repl {
-	u8 msg_type;
-	u8 pkt_id;
-	u8 len;
-} __packed;
-
-#define GET_IDENT 0xE0
-
-struct get_ident_repl {
-	struct header_repl header;
-	u8 module_id;
-	u8 cmd_id;
-	u8 type_name;
-	u8 hw_major;
-	u8 hw_minor;
-	u8 sw_major;
-	u8 sw_minor;
-	u8 sw_sub;
-} __packed;
-
-#define GET_ERROR 0xE2
-
-struct get_error_repl {
-	struct header_repl header;
-	u8 module_id;
-	u8 cmd_id;
-	u8 last_cmd_id;
-	u8 error;
-	u8 status;
-} __packed;
-
-#define GET_DEV_STATUS 0x80
-
-enum dev_state {STATE_BOOT, STATE_IDLE, STATE_STBY, STATE_LDAG, STATE_PLAY,
-			STATE_PROT, STATE_SDWN, STATE_CLFA, STATE_NONE };
-
-struct get_dev_status_repl {
-	struct header_repl header;
-	u8 module_id;
-	u8 cmd_id;
-	u8 state;
-} __packed;
 
 /* Helpers */
 #define CHNL_MASK(channels) (u8)((0x00FF << channels) >> 8)
