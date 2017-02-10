7c7fd408a0c33ca1c6cc5570f8d21d97431652e8
Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
2017-02-09 18:40:32 -0800

    thermal:pch: Add CNL support
    Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>

--- drivers/thermal/intel_pch_thermal.c
+++ drivers/thermal/intel_pch_thermal.c
@@ -30,6 +30,7 @@
 #define PCH_THERMAL_DID_WPT	0x9CA4 /* Wildcat Point */
 #define PCH_THERMAL_DID_SKL	0x9D31 /* Skylake PCH */
 #define PCH_THERMAL_DID_SKL_H	0xA131 /* Skylake PCH 100 series */
+#define PCH_THERMAL_DID_CNL	0x9Df9 /* CNL PCH */
 
 /* Wildcat Point-LP  PCH Thermal registers */
 #define WPT_TEMP	0x0000	/* Temperature */
@@ -278,6 +279,7 @@ enum board_ids {
 	board_hsw,
 	board_wpt,
 	board_skl,
+	board_cnl,
 };
 
 static const struct board_info {
@@ -294,6 +296,10 @@ static const struct board_info {
 	},
 	[board_skl] = {
 		.name = "pch_skylake",
+		.ops = &pch_dev_ops_wpt,
+	},
+	[board_cnl] = {
+		.name = "pch_cannonlake",
 		.ops = &pch_dev_ops_wpt,
 	},
 };
@@ -398,6 +404,8 @@ static const struct pci_device_id intel_pch_thermal_id[] = {
 		.driver_data = board_skl, },
 	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCH_THERMAL_DID_SKL_H),
 		.driver_data = board_skl, },
+	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCH_THERMAL_DID_CNL),
+		.driver_data = board_cnl, },
 	{ 0, },
 };
 MODULE_DEVICE_TABLE(pci, intel_pch_thermal_id);
