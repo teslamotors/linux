e0675727dbba1b3d6bc51ed0ac0c96358f8fd8c0
Shreyas NC <shreyas.nc@intel.com>
2017-03-08 03:08:18 +0530

    ASoC: Intel: Skylake: Define tokens for aggregation
    To support aggregation, define tokens for aggregation id,
    masters participating in aggregation, link id and channel mask.
    
    Change-Id: Ib7e3f5a3aec4d8a6e2dec1b1f045c8078a3ea958
    Signed-off-by: Shreyas NC <shreyas.nc@intel.com>
    Reviewed-on: https://git-gar-1.devtools.intel.com/gerrit/14208
    Reviewed-by: Singh, Guneshwor O <guneshwor.o.singh@intel.com>
    Reviewed-by: Jayanti, Satya Charitardha <satya.charitardha.jayanti@intel.com>
    Reviewed-by: Prodduvaka, Leoni <leoni.prodduvaka@intel.com>
    Reviewed-by: Prusty, Subhransu S <subhransu.s.prusty@intel.com>
    Reviewed-by: R, Dharageswari <dharageswari.r@intel.com>
    Reviewed-by: Koul, Vinod <vinod.koul@intel.com>
    Reviewed-by: Diwakar, Praveen <praveen.diwakar@intel.com>
    Reviewed-by: Kale, Sanyog R <sanyog.r.kale@intel.com>
    Tested-by: Avati, Santosh Kumar <santosh.kumar.avati@intel.com>

--- include/uapi/sound/snd_sst_tokens.h
+++ include/uapi/sound/snd_sst_tokens.h
@@ -222,6 +222,17 @@
  * %SKL_TKN_MM_U32_NUM_IN_FMT:  Number of input formats
  * %SKL_TKN_MM_U32_NUM_OUT_FMT: Number of output formats
  *
+ * %SKL_TKN_U32_AGG_NUM_MASTERS:
+ *                              Number of aggregated masters
+ *
+ * %SKL_TKN_U32_AGG_LINK_ID:    Aggregated master's instance id
+ *
+ * %SKL_TKN_U32_AGG_CH_MASK:    Represents channels driven by the master
+ *
+ * %SKL_TKN_U32_AGG_ID:         Aggregation id is a non zero identifier to
+ *                              indicate if this endpoint is participating
+ *                              in aggregation.
+ *
  * module_id and loadable flags dont have tokens as these values will be
  * read from the DSP FW manifest
  *
@@ -309,7 +320,12 @@ enum SKL_TKNS {
 	SKL_TKN_MM_U32_NUM_IN_FMT,
 	SKL_TKN_MM_U32_NUM_OUT_FMT,
 
-	SKL_TKN_MAX = SKL_TKN_MM_U32_NUM_OUT_FMT,
+	SKL_TKN_U32_AGG_NUM_MASTERS,
+	SKL_TKN_U32_AGG_LINK_ID,
+	SKL_TKN_U32_AGG_CH_MASK,
+	SKL_TKN_U32_AGG_ID,
+
+	SKL_TKN_MAX = SKL_TKN_U32_AGG_ID,
 };
 
 #endif
