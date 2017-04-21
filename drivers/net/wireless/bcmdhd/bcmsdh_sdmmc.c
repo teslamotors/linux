45836a460271c78595f7ec6331932f3ffae63506
Mattias Nissler <mnissler@google.com>
2017-04-21 23:34:26 +0000

    VENDOR: broadcom: bcmdhd: Use sdio_reset_comm from common kernel
    A proper definition of sdio_reset_comm() was brought in when merging
    Android-specific common kernel changes into Brillo's common kernel. We
    can now the hack compile fix hack in bcmsdh_sdmmc.c.
    
    Bug: 25923642
    Patchset: wlan0 up on kylin board, iw is normal connect the AP.
    
    Signed-off-by: Mattias Nissler <mnissler@google.com>
    Change-Id: I7fea434b7f173b25acab2942eba106c2da542457

--- drivers/net/wireless/bcmdhd/bcmsdh_sdmmc.c
+++ drivers/net/wireless/bcmdhd/bcmsdh_sdmmc.c
@@ -62,14 +62,7 @@ static void IRQHandler(struct sdio_func *func);
 static void IRQHandlerF2(struct sdio_func *func);
 #endif /* !defined(OOB_INTR_ONLY) */
 static int sdioh_sdmmc_get_cisaddr(sdioh_info_t *sd, uint32 regaddr);
-#if 0 
 extern int sdio_reset_comm(struct mmc_card *card);
-#else
-int sdio_reset_comm(struct mmc_card *card)
-{
-	return 0;
-}
-#endif
 
 #define DEFAULT_SDIO_F2_BLKSIZE		512
 #ifndef CUSTOM_SDIO_F2_BLKSIZE
