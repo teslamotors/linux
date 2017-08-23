3fa08d8d3fa94abcb7bd8087a89f2212562b2be9
Tomas Winkler <tomas.winkler@intel.com>
2017-08-23 10:58:55 +0300

    mei: me: add cannon point iTouch device ids
    Add cannon point device ids: for HECI3 (itouch) device.
    
    Change-Id: Ifa9d989e7321494417487ffee3eaac7261949646
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>

--- drivers/misc/mei/hw-me-regs.h
+++ drivers/misc/mei/hw-me-regs.h
@@ -134,8 +134,10 @@
 
 #define MEI_DEV_ID_CNP_LP     0x9DE0  /* Cannon Point LP */
 #define MEI_DEV_ID_CNP_LP_2   0x9DE1  /* Cannon Point LP 2 */
+#define MEI_DEV_ID_CNP_LP_4   0x9DE4  /* Cannon Point LP 4 (iTouch) */
 #define MEI_DEV_ID_CNP_H      0xA360  /* Cannon Point H */
 #define MEI_DEV_ID_CNP_H_2    0xA361  /* Cannon Point H 2 */
+#define MEI_DEV_ID_CNP_H_4    0xA364  /* Cannon Point H 4 (iTouch) */
 
 /*
  * MEI HW Section
