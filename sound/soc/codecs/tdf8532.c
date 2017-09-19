695c8779cf86c8362689348d8136ac1bc0ad6abb
Craig Kewley <craigx.kewley@intel.com>
2017-09-19 12:52:30 +0100

    ASoC: TDF8532: update license and author info
    Update license and author info.
    
    Signed-off-by: Craig Kewley <craigx.kewley@intel.com>

--- sound/soc/codecs/tdf8532.c
+++ sound/soc/codecs/tdf8532.c
@@ -1,13 +1,12 @@
 /*
  * tdf8532.c  --  driver for NXP Semiconductors TDF8532
  *
- * Copyright (C) 2016 Intel Corp.
- * Author: Steffen Wagner <steffen.wagner@intel.com>
+ * Copyright (C) 2017 Intel Corp.
  *
- *  This program is free software; you can redistribute  it and/or modify it
- *  under  the terms of  the GNU General  Public License as published by the
- *  Free Software Foundation;  either version 2 of the  License, or (at your
- *  option) any later version.
+ * This program is free software; you can redistribute  it and/or modify it
+ * under  the terms of  the GNU General  Public License as published by the
+ * Free Software Foundation;  either version 2 of the  License, or (at your
+ * option) any later version.
  *
  */
 #include <linux/module.h>
@@ -445,4 +444,5 @@ module_i2c_driver(tdf8532_i2c_driver);
 
 MODULE_DESCRIPTION("ASoC NXP Semiconductors TDF8532 driver");
 MODULE_AUTHOR("Steffen Wagner <steffen.wagner@intel.com>");
+MODULE_AUTHOR("Craig Kewley <craigx.kewley@intel.com>");
 MODULE_LICENSE("GPL");
