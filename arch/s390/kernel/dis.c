c06a786de8789dc43f320c958a03d8c5073396f8
Vasily Gorbik <gor@linux.vnet.ibm.com>
2017-11-23 10:50:23 +0100

    s390/disassembler: correct disassembly lines alignment
    commit 26f4e759ef9b8a2bab1823d692ed6d56d40b66e3 upstream.
    
    176.718956 Krnl Code: 00000000004d38b0: a54c0018        llihh   %r4,24
    176.718956 	   00000000004d38b4: b9080014        agr     %r1,%r4
               ^
    Using a tab to align disassembly lines which follow the first line with
    "Krnl Code: " doesn't always work, e.g. if there is a prefix (timestamp
    or syslog prefix) which is not 8 chars aligned. Go back to alignment
    with spaces.
    
    Fixes: b192571d1ae3 ("s390/disassembler: increase show_code buffer size")
    Signed-off-by: Vasily Gorbik <gor@linux.vnet.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/s390/kernel/dis.c
+++ arch/s390/kernel/dis.c
@@ -2018,7 +2018,7 @@ void show_code(struct pt_regs *regs)
 		start += opsize;
 		pr_cont("%s", buffer);
 		ptr = buffer;
-		ptr += sprintf(ptr, "\n\t  ");
+		ptr += sprintf(ptr, "\n          ");
 		hops++;
 	}
 	pr_cont("\n");
