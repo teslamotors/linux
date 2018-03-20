intel/linux-intel-lts

usb: xhci: phy may be initialized by xhci or by dwc3
On the BXT-P both drivers use the same phy and mux.
The phy is controlled by platform driver intel_usb_dr_phy
and the platform device must be exist to before
any of the xhci or dwc3 driver initialize. Otherwise phy
won't be managed at all.
This patch instantiate intel_usb_dr_phy platform device
by any of the drivers xhci and dwc3 whichever initialize
as first.

Change-Id: I2c8ce2356ed6af913440b60c8e85639a6c30246e
Tracked-On: https://jira01.devtools.intel.com/browse/OAM-56867
Signed-off-by: Dominik Bozek <dominikx.bozek@intel.com>
Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

drivers/usb/dwc3/dwc3-pci.c
@@ -25,6 +25,7 @@
 #include <linux/gpio/consumer.h>
 #include <linux/acpi.h>
 #include <linux/delay.h>
+#include <linux/usb/phy.h>
 
 #define PCI_DEVICE_ID_SYNOPSYS_HAPSUSB3		0xabcd
 #define PCI_DEVICE_ID_SYNOPSYS_HAPSUSB3_AXI	0xabce
@@ -114,16 +115,27 @@ static int dwc3_pci_quirks(struct dwc3_pci *dwc_pci, struct platform_device *dwc
 		    pdev->device == PCI_DEVICE_ID_INTEL_BXT_M ||
 		    pdev->device == PCI_DEVICE_ID_INTEL_APL ) {
 
-			dwc_pci->usb2_phy = platform_device_alloc(
-							"intel_usb_dr_phy", 0);
-			if (!dwc_pci->usb2_phy)
-				return -ENOMEM;
-
-			dwc_pci->usb2_phy->dev.parent = &pdev->dev;
-	                ret = platform_device_add(dwc_pci->usb2_phy);
-		        if (ret) {
-			        platform_device_put(dwc_pci->usb2_phy);
-				return ret;
+			if (IS_ERR_OR_NULL(usb_get_phy(USB_PHY_TYPE_USB2))) {
+				/* Unable to get phy, very likely intel_usb_dr_phy
+				 * platform device not yet allocated. Possible
+				 * race with another drivers.
+				 */
+				dwc_pci->usb2_phy = platform_device_alloc(
+								"intel_usb_dr_phy", 0);
+				if (dwc_pci->usb2_phy) {
+					dwc_pci->usb2_phy->dev.parent = &pdev->dev;
+					ret = platform_device_add(dwc_pci->usb2_phy);
+					if (ret) {
+						platform_device_put(dwc_pci->usb2_phy);
+						return ret;
+					}
+				} else if (IS_ERR_OR_NULL(usb_get_phy(USB_PHY_TYPE_USB2))) {
+					/* In case of a race and another driver allocate
+					 * intel_usb_dr_phy platform device earlier, check
+					 * whether phy is already available. If not return error.
+					 */
+					return -ENOMEM;
+				}
 			}
 		}
 

teslamotors/linux

drivers/usb/dwc3/dwc3-pci.c
@@ -25,6 +25,7 @@
 #include <linux/gpio/consumer.h>
 #include <linux/acpi.h>
 #include <linux/delay.h>
+#include <linux/usb/phy.h>
 
 #define PCI_DEVICE_ID_SYNOPSYS_HAPSUSB3		0xabcd
 #define PCI_DEVICE_ID_SYNOPSYS_HAPSUSB3_AXI	0xabce
@@ -129,16 +130,27 @@ static int dwc3_pci_quirks(struct dwc3_pci *dwc)
 		    pdev->device == PCI_DEVICE_ID_INTEL_BXT_M ||
 		    pdev->device == PCI_DEVICE_ID_INTEL_APL ) {
 
-			dwc->usb2_phy = platform_device_alloc(
-							"intel_usb_dr_phy", 0);
-			if (!dwc->usb2_phy)
-				return -ENOMEM;
-
-			dwc->usb2_phy->dev.parent = &pdev->dev;
-	                ret = platform_device_add(dwc->usb2_phy);
-		        if (ret) {
-			        platform_device_put(dwc->usb2_phy);
-				return ret;
+			if (IS_ERR_OR_NULL(usb_get_phy(USB_PHY_TYPE_USB2))) {
+				/* Unable to get phy, very likely intel_usb_dr_phy
+				 * platform device not yet allocated. Possible
+				 * race with another drivers.
+				 */
+				dwc->usb2_phy = platform_device_alloc(
+								"intel_usb_dr_phy", 0);
+				if (dwc->usb2_phy) {
+					dwc->usb2_phy->dev.parent = &pdev->dev;
+					ret = platform_device_add(dwc->usb2_phy);
+					if (ret) {
+						platform_device_put(dwc->usb2_phy);
+						return ret;
+					}
+				} else if (IS_ERR_OR_NULL(usb_get_phy(USB_PHY_TYPE_USB2))) {
+					/* In case of a race and another driver allocate
+					 * intel_usb_dr_phy platform device earlier, check
+					 * whether phy is already available. If not return error.
+					 */
+					return -ENOMEM;
+				}
 			}
 		}
 