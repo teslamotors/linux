06de107e7581ccc50ce65c763ccbd6d37f115252
Damien Lespiau <damien.lespiau@intel.com>
2018-05-07 13:32:07 -0700

    drm: Introduce the blend-func property (v3)
    We'd like to be able to program the blending modes of display planes.
    Ville suggested to use something similar to the GL blend states, which
    does seem like a good idea.
    
    For now, we only consider blend factors, but room is left for
    extensions: blend equation, separate rgb/alpha blend factors, blend
    color.
    
    V2: Added the belnd func property support in get property.
    
    v3 (by Matt):
     - Move checking to intel_plane_atomic_check()
     - Reject garbage bits in the blending function value
     - Add DRM_DEBUG_KMS() messages on check failures
     - Make property atomic-only
    
    Suggested-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>
    Signed-off-by: Vandita Kulkarni <vandita.kulkarni@intel.com>
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>
    Signed-off-by: Bob Paauwe <bob.j.paauwe@intel.com>
    Signed-off-by: Paauwe, Bob J <bob.j.paauwe@intel.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -783,6 +783,8 @@ static int drm_atomic_plane_set_property(struct drm_plane *plane,
 		state->rotation = val;
 	} else if (property == plane->zpos_property) {
 		state->zpos = val;
+	} else if (property == config->prop_blend_func) {
+		state->blend_mode.func = val;
 	} else if (plane->funcs->atomic_set_property) {
 		return plane->funcs->atomic_set_property(plane, state,
 				property, val);
@@ -842,6 +844,8 @@ drm_atomic_plane_get_property(struct drm_plane *plane,
 		*val = state->rotation;
 	} else if (property == plane->zpos_property) {
 		*val = state->zpos;
+	} else if (property == config->prop_blend_func) {
+		*val = state->blend_mode.func;
 	} else if (plane->funcs->atomic_get_property) {
 		return plane->funcs->atomic_get_property(plane, state, property, val);
 	} else {
