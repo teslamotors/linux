b23ae8be2593d6db9ac1c753349a16a98f033764
Gopal, Puunithaaraj <puunithaaraj.gopal@intel.com>
2018-05-17 00:28:53 +0800

    media: platform: Change I2C bus ID from 0 to 3
    Change-Id: I1750aedaac4e128d7a99f2b4d96e19ddccf2d0a2
    Signed-off-by: Gopal, Puunithaaraj <puunithaaraj.gopal@intel.com>

--- drivers/media/platform/intel/ipu4-ici-bxt-p-pdata.c
+++ drivers/media/platform/intel/ipu4-ici-bxt-p-pdata.c
@@ -48,7 +48,7 @@ static struct ipu_isys_subdev_info adv7481_hdmi_crl_sd_lite = {
 #ifdef CONFIG_VIDEO_INTEL_UOS
 		.i2c_adapter_id = 6,
 #else
-		.i2c_adapter_id = 0,
+		.i2c_adapter_id = 3,
 #endif
 	}
 };
@@ -79,7 +79,7 @@ static struct ipu_isys_subdev_info adv7481_cvbs_crl_sd_lite = {
 #ifdef CONFIG_VIDEO_INTEL_UOS
 		.i2c_adapter_id = 6,
 #else
-		.i2c_adapter_id = 0,
+		.i2c_adapter_id = 3,
 #endif
 	}
 };
