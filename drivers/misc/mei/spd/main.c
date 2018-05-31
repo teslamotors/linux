9a579e7510cb493a653b66cd7185d15ebf48210a
Tomas Winkler <tomas.winkler@intel.com>
2018-05-31 22:55:53 +0300

    Revert "mei: spd: connect to the rpmb subsystem"
    This reverts commit 131ea7c031fd8c361b7a2d9423286c18700232cb.
    
    Change-Id: I160a0b025c6102014ae486b49d122184385e1685

--- drivers/misc/mei/spd/main.c
+++ drivers/misc/mei/spd/main.c
@@ -63,7 +63,6 @@ static int mei_spd_probe(struct mei_cl_device *cldev,
 
 	spd_dbg(spd, "protocol version %d\n", ver);
 	mei_spd_gpp_prepare(spd);
-	mei_spd_rpmb_prepare(spd);
 	mutex_lock(&spd->lock);
 	ret = mei_spd_cmd_init_req(spd);
 	mutex_unlock(&spd->lock);
@@ -91,7 +90,6 @@ static int mei_spd_remove(struct mei_cl_device *cldev)
 	if (spd->state == MEI_SPD_STATE_RUNNING) {
 		spd->state = MEI_SPD_STATE_STOPPING;
 		mei_spd_gpp_exit(spd);
-		mei_spd_rpmb_exit(spd);
 		mutex_lock(&spd->lock);
 		mei_spd_cmd_storage_status_req(spd);
 		mutex_unlock(&spd->lock);
