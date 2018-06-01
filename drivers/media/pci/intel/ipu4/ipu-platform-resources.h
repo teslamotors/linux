4a7f9d470fcf577141916335f503d04a95983776
Wang Zaikuo <zaikuo.wang@intel.com>
2018-06-01 15:56:50 +0800

    media: intel-ipu4: psys: move resource tables to abi file
    Message for Open Source:
    Those tables are only used by ABI, so move to ABI specific file
    
    Message for Internal:
    [Issue/Feature] This is an improvement.
    [Root Cause/Changes] Same as above.
    
    Change-Id: I78ea5415938f277196ebd0e4340c916f44ce4a5d
    Tracked-On: #JKRLCSN-256
    Signed-off-by: Wang Zaikuo <zaikuo.wang@intel.com>
    Signed-off-by: Meng Wei <wei.meng@intel.com>

--- drivers/media/pci/intel/ipu4/ipu-platform-resources.h
+++ drivers/media/pci/intel/ipu4/ipu-platform-resources.h
@@ -220,11 +220,6 @@ int ipu_psys_move_resources(const struct device *dev,
 void ipu_psys_free_resources(struct ipu_psys_resource_alloc *alloc,
 			     struct ipu_psys_resource_pool *pool);
 
-extern const u32 ipu_fw_psys_cell_types[];
-extern const u16 ipu_fw_num_dev_channels[];
-extern const u16 ipu_fw_psys_mem_size[];
-extern const enum ipu_mem_id ipu_fw_psys_cell_mem
-	[IPU_FW_PSYS_N_CELL_ID][IPU_FW_PSYS_N_MEM_TYPE_ID];
 extern const struct ipu_fw_resource_definitions *res_defs;
 
 #endif /* IPU_PLATFORM_RESOURCES_H */
