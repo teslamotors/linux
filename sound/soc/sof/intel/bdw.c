bf8669b3b1cc47af23f4fa1b01cca132e1a063e2
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2018-06-17 22:07:52 +0100

    ASoC: SOF: Intel BDW platform DAIs
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>
    Signed-off-by: Keyon Jie <yang.jie@linux.intel.com>

--- sound/soc/sof/intel/bdw.c
+++ sound/soc/sof/intel/bdw.c
@@ -709,6 +709,32 @@ static int bdw_remove(struct snd_sof_dev *sdev)
 	return 0;
 }
 
+#define BDW_FORMATS (SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S24_LE | \
+	SNDRV_PCM_FMTBIT_S32_LE)
+
+/* Broadwell DAIs */
+static struct snd_soc_dai_driver bdw_dai[] = {
+{
+	.name = "ssp0-port",
+	.playback = SOF_DAI_STREAM("ssp0 Tx", 1, 8,
+				   SNDRV_PCM_RATE_8000_192000, BDW_FORMATS),
+	.capture = SOF_DAI_STREAM("ssp0 Rx", 1, 8,
+				  SNDRV_PCM_RATE_8000_192000, BDW_FORMATS),
+},
+{
+	.name = "ssp1-port",
+	.playback = SOF_DAI_STREAM("ssp1 Tx", 1, 8,
+				   SNDRV_PCM_RATE_8000_192000, BDW_FORMATS),
+	.capture = SOF_DAI_STREAM("ssp1 Rx", 1, 8,
+				  SNDRV_PCM_RATE_8000_192000, BDW_FORMATS),
+},
+};
+
+struct snd_sof_dai_drv bdw_dai_drv = {
+	.drv = bdw_dai,
+	.num_drv = ARRAY_SIZE(bdw_dai)
+};
+
 /* broadwell ops */
 struct snd_sof_dsp_ops sof_bdw_ops = {
 	/*Device init */
@@ -750,6 +776,9 @@ struct snd_sof_dsp_ops sof_bdw_ops = {
 
 	/*Firmware loading */
 	.load_firmware	= snd_sof_load_firmware_memcpy,
+
+	/* DAI drivers */
+	.dai_drv = &bdw_dai_drv,
 };
 EXPORT_SYMBOL(sof_bdw_ops);
 
