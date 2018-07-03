d13f0bfa5cbe8ba1f6e926edaf9761fff23d3cc0
Matt Roper <matthew.d.roper@intel.com>
2018-07-03 14:19:41 -0700

    Add IOTG kernel identifier
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -1426,6 +1426,9 @@ int i915_driver_load(struct pci_dev *pdev, const struct pci_device_id *ent)
 
 	intel_runtime_pm_put(dev_priv);
 
+	printk(KERN_INFO "IOTG i915 driver 2018y-08m-27d-09h-59m-48s -0700\n");
+	printk(KERN_INFO "42 patches carried on top of PK commit 032e009f\n");
+
 	return 0;
 
 out_cleanup_hw:
