8ee12d3a0fe15a58d492da17a3cf6c3d96e67271
Fei Jiang <fei.jiang@intel.com>
2018-07-24 09:20:23 +0800

    drm/i915/gvt: pvmmio optimization for plane update
    It is performance optimization to reduce plane related register trap
    counter. When update plane, multiple plane related registers are updated
    together, optimize it to firstly cache all register values in share page,
    then only PLANE_SURF register writing is trapped. Plane pvmmio level is
    PVMMIO_PLANE_UPDATE.
    Patch for both SOS and UOS.
    
    V2: add memset tmp_plane to be more safer and add more commit description
    
    Signed-off-by: Fei Jiang <fei.jiang@intel.com>
    Reviewed-by: Min He <min.he@intel.com>
    Reviewed-by: Zhao Yakui <yakui.zhao@intel.com>

--- drivers/gpu/drm/i915/i915_pvinfo.h
+++ drivers/gpu/drm/i915/i915_pvinfo.h
@@ -55,13 +55,37 @@ enum vgt_g2v_type {
 	VGT_G2V_MAX,
 };
 
+#define PLANE_COLOR_CTL_BIT	(1 << 0)
+#define PLANE_KEY_BIT		(1 << 1)
+#define PLANE_SCALER_BIT	(1 << 2)
+
+struct pv_plane_update {
+	u32 flags;
+	u32 plane_color_ctl;
+	u32 plane_key_val;
+	u32 plane_key_max;
+	u32 plane_key_msk;
+	u32 plane_offset;
+	u32 plane_stride;
+	u32 plane_size;
+	u32 plane_aux_dist;
+	u32 plane_aux_offset;
+	u32 ps_ctrl;
+	u32 ps_pwr_gate;
+	u32 ps_win_ps;
+	u32 ps_win_sz;
+	u32 plane_pos;
+	u32 plane_ctl;
+};
+
 /* shared page(4KB) between gvt and VM, located at the first page next
  * to MMIO region(2MB size normally).
  */
 struct gvt_shared_page {
 	u32 elsp_data[4];
 	u32 reg_addr;
-	u32 rsvd2[0x400 - 5];
+	struct pv_plane_update pv_plane;
+	u32 rsvd2[0x400 - 21];
 };
 
 #define VGPU_PVMMIO(vgpu) vgpu_vreg(vgpu, vgtif_reg(enable_pvmmio))
@@ -76,6 +100,7 @@ struct gvt_shared_page {
  */
 enum pvmmio_levels {
 	PVMMIO_ELSP_SUBMIT = 0x1,
+	PVMMIO_PLANE_UPDATE = 0x2,
 };
 
 struct vgt_if {
