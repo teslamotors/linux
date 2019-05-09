8051c91f21060c6dd26f31944812963d83dc49e4
qianmenx <qianx.meng@intel.com>
2019-05-09 10:42:25 +0800

    media: intel-ipu4: update metadata info for x3a
    Message for Open Source:
    update metadata info for x3a
    
    
    Change-Id: Ic7555778372eff8e06af416a858c8aa0faf54655
    Tracked-On: #PKT-2352
    Signed-off-by: qianmenx <qianx.meng@intel.com>
    Signed-off-by: Meng Wei <wei.meng@intel.com>

--- drivers/media/i2c/crlmodule/crl_ox03a10_common.h
+++ drivers/media/i2c/crlmodule/crl_ox03a10_common.h
@@ -530,8 +530,15 @@ static struct crl_register_write_rep ox03a10_1920_1088_12DCG[] = {
 	{ 0x460a, CRL_REG_LEN_08BIT, 0x0e },
 	/* embedded data */
 	{ 0x3208, CRL_REG_LEN_08BIT, 0x04 },
-	{ 0x3800, CRL_REG_LEN_08BIT, 0x03 },
-	{ 0x4800, CRL_REG_LEN_08BIT, 0x02 },
+	{ 0x483e, CRL_REG_LEN_08BIT, 0x02 },/* frame counter */
+	{ 0x3501, CRL_REG_LEN_08BIT, 0x02 },/* DCG exp */
+	{ 0x3581, CRL_REG_LEN_08BIT, 0x02 },/* VS exp */
+	{ 0x350a, CRL_REG_LEN_08BIT, 0x02 },/* HCG Dgain */
+	{ 0x354a, CRL_REG_LEN_08BIT, 0x02 },/* LCG Dgain */
+	{ 0x358a, CRL_REG_LEN_08BIT, 0x02 },/* VS Dgain */
+	{ 0x3508, CRL_REG_LEN_08BIT, 0x02 },/* HCG Again */
+	{ 0x3548, CRL_REG_LEN_08BIT, 0x02 },/* LCG Again */
+	{ 0x3588, CRL_REG_LEN_08BIT, 0x02 },/* VS Again */
 	{ 0x3208, CRL_REG_LEN_08BIT, 0x14 },
 	{ 0x3208, CRL_REG_LEN_08BIT, 0x05 },
 	{ 0x5000, CRL_REG_LEN_08BIT, 0x10 },
