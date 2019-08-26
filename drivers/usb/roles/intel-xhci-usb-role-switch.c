78e9269227905d57f1ad8a40cde108a819a31838
Iulian Mocanu <iulian.mocanu@intel.com>
2019-08-26 13:32:32 +0200

    Revert "[REVERTME] usb: roles: intel_xhci: Set device mode as default."
    This reverts commit 544b6dce1f4445bb3397d7c2b020b1fd4325ac07.
    
    RTC : 294164

--- drivers/usb/roles/intel-xhci-usb-role-switch.c
+++ drivers/usb/roles/intel-xhci-usb-role-switch.c
@@ -182,10 +182,6 @@ static int intel_xhci_usb_probe(struct platform_device *pdev)
 	pm_runtime_set_active(dev);
 	pm_runtime_enable(dev);
 
-	intel_xhci_usb_set_role(dev, USB_ROLE_HOST);
-	msleep(10);
-	intel_xhci_usb_set_role(dev, USB_ROLE_DEVICE);
-
 	/* Override USB OTG port default role. */
 	switch (default_role) {
 	case USB_ROLE_HOST:
