caaefbf823ab7f5c0926fac62f0aa171ae5beeea
Vignesh Raghavendra <vigneshr@ti.com>
2020-06-01 12:34:41 +0530

    mtd: spi-nor: cadence-quadspi: Handle probe deferral while requesting DMA channel
    [ Upstream commit 935da5e5100f57d843cac4781b21f1c235059aa0 ]
    
    dma_request_chan_by_mask() can throw EPROBE_DEFER if DMA provider
    is not yet probed. Currently driver just falls back to using PIO mode
    (which is less efficient) in this case. Instead return probe deferral
    error as is so that driver will be re probed once DMA provider is
    available.
    
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>
    Reviewed-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Acked-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    Link: https://lore.kernel.org/r/20200601070444.16923-6-vigneshr@ti.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Stable-dep-of: 2087e85bb66e ("spi: cadence-quadspi: fix suspend-resume implementations")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/mtd/spi-nor/cadence-quadspi.c
+++ drivers/mtd/spi-nor/cadence-quadspi.c
@@ -1168,7 +1168,7 @@ static void cqspi_controller_init(struct cqspi_st *cqspi)
 	cqspi_controller_enable(cqspi, 1);
 }
 
-static void cqspi_request_mmap_dma(struct cqspi_st *cqspi)
+static int cqspi_request_mmap_dma(struct cqspi_st *cqspi)
 {
 	dma_cap_mask_t mask;
 
@@ -1177,11 +1177,16 @@ static void cqspi_request_mmap_dma(struct cqspi_st *cqspi)
 
 	cqspi->rx_chan = dma_request_chan_by_mask(&mask);
 	if (IS_ERR(cqspi->rx_chan)) {
-		dev_err(&cqspi->pdev->dev, "No Rx DMA available\n");
+		int ret = PTR_ERR(cqspi->rx_chan);
+
+		if (ret != -EPROBE_DEFER)
+			dev_err(&cqspi->pdev->dev, "No Rx DMA available\n");
 		cqspi->rx_chan = NULL;
-		return;
+		return ret;
 	}
 	init_completion(&cqspi->rx_dma_complete);
+
+	return 0;
 }
 
 static int cqspi_setup_flash(struct cqspi_st *cqspi, struct device_node *np)
@@ -1265,8 +1270,11 @@ static int cqspi_setup_flash(struct cqspi_st *cqspi, struct device_node *np)
 			dev_dbg(nor->dev, "using direct mode for %s\n",
 				mtd->name);
 
-			if (!cqspi->rx_chan)
-				cqspi_request_mmap_dma(cqspi);
+			if (!cqspi->rx_chan) {
+				ret = cqspi_request_mmap_dma(cqspi);
+				if (ret == -EPROBE_DEFER)
+					goto err;
+			}
 		}
 	}
 
