0a55f346e0e5f90ed9204b093b9c0d4f1aa04439
Qu Wenruo <wqu@suse.com>
2020-06-10 09:04:44 +0800

    btrfs: qgroup: catch reserved space leaks at unmount time
    [ Upstream commit 5958253cf65de42493f17f36877a901486a90365 ]
    
    Before this patch, qgroup completely relies on per-inode extent io tree
    to detect reserved data space leak.
    
    However previous bug has already shown how release page before
    btrfs_finish_ordered_io() could lead to leak, and since it's
    QGROUP_RESERVED bit cleared without triggering qgroup rsv, it can't be
    detected by per-inode extent io tree.
    
    So this patch adds another (and hopefully the final) safety net to catch
    qgroup data reserved space leak.  At least the new safety net catches
    all the leaks during development, so it should be pretty useful in the
    real world.
    
    Reviewed-by: Josef Bacik <josef@toxicpanda.com>
    Signed-off-by: Qu Wenruo <wqu@suse.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>
    Stable-dep-of: 8a4a0b2a3eaf ("btrfs: fix race between quota disable and relocation")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/btrfs/disk-io.c
+++ fs/btrfs/disk-io.c
@@ -4106,6 +4106,11 @@ void close_ctree(struct btrfs_fs_info *fs_info)
 	ASSERT(list_empty(&fs_info->delayed_iputs));
 	set_bit(BTRFS_FS_CLOSING_DONE, &fs_info->flags);
 
+	if (btrfs_check_quota_leak(fs_info)) {
+		WARN_ON(IS_ENABLED(CONFIG_BTRFS_DEBUG));
+		btrfs_err(fs_info, "qgroup reserved space leaked");
+	}
+
 	btrfs_free_qgroup_config(fs_info);
 	ASSERT(list_empty(&fs_info->delalloc_roots));
 
