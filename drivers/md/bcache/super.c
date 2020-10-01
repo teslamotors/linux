38a6dd2b68f9ee7b90dc312c8335854d2bd70c0b
Coly Li <colyli@suse.de>
2020-10-01 14:50:45 +0800

    bcache: remove 'int n' from parameter list of bch_bucket_alloc_set()
    [ Upstream commit 17e4aed8309ff28670271546c2c3263eb12f5eb6 ]
    
    The parameter 'int n' from bch_bucket_alloc_set() is not cleared
    defined. From the code comments n is the number of buckets to alloc, but
    from the code itself 'n' is the maximum cache to iterate. Indeed all the
    locations where bch_bucket_alloc_set() is called, 'n' is alwasy 1.
    
    This patch removes the confused and unnecessary 'int n' from parameter
    list of  bch_bucket_alloc_set(), and explicitly allocates only 1 bucket
    for its caller.
    
    Signed-off-by: Coly Li <colyli@suse.de>
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Stable-dep-of: 80fca8a10b60 ("bcache: Fix __bch_btree_node_alloc to make the failure behavior consistent")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -428,7 +428,7 @@ static int __uuid_write(struct cache_set *c)
 	closure_init_stack(&cl);
 	lockdep_assert_held(&bch_register_lock);
 
-	if (bch_bucket_alloc_set(c, RESERVE_BTREE, &k.key, 1, true))
+	if (bch_bucket_alloc_set(c, RESERVE_BTREE, &k.key, true))
 		return 1;
 
 	SET_KEY_SIZE(&k.key, c->sb.bucket_size);
