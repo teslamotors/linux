24cf809170ed7332d4f8ed7d6f6db256d1db9c64
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2021-01-17 15:26:44 +0100

    cpufreq: brcmstb-avs-cpufreq: Fix resource leaks in ->remove()
    [ Upstream commit 3657f729b6fb5f2c0bf693742de2dcd49c572aa1 ]
    
    If 'cpufreq_unregister_driver()' fails, just WARN and continue, so that
    other resources are freed.
    
    Fixes: de322e085995 ("cpufreq: brcmstb-avs-cpufreq: AVS CPUfreq driver for Broadcom STB SoCs")
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    [ Viresh: Updated Subject ]
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/cpufreq/brcmstb-avs-cpufreq.c
+++ drivers/cpufreq/brcmstb-avs-cpufreq.c
@@ -1033,8 +1033,7 @@ static int brcm_avs_cpufreq_remove(struct platform_device *pdev)
 	int ret;
 
 	ret = cpufreq_unregister_driver(&brcm_avs_driver);
-	if (ret)
-		return ret;
+	WARN_ON(ret);
 
 	brcm_avs_cpufreq_debug_exit(pdev);
 
