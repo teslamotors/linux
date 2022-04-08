1e8716a5c0873c9f824e052ccc407db04560ad79
Mathias Nyman <mathias.nyman@linux.intel.com>
2022-04-08 16:48:23 +0300

    xhci: increase usb U3 -> U0 link resume timeout from 100ms to 500ms
    commit 33597f0c48be0836854d43c577e35c8f8a765a7d upstream.
    
    The first U3 wake signal by the host may be lost if the USB 3 connection is
    tunneled over USB4, with a runtime suspended USB4 host, and firmware
    implemented connection manager.
    
    Specs state the host must wait 100ms (tU3WakeupRetryDelay) before
    resending a U3 wake signal if device doesn't respond, leading to U3 -> U0
    link transition times around 270ms in the tunneled case.
    
    Fixes: 0200b9f790b0 ("xhci: Wait until link state trainsits to U0 after setting USB_SS_PORT_LS_U0")
    Cc: stable@vger.kernel.org
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Link: https://lore.kernel.org/r/20220408134823.2527272-4-mathias.nyman@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/host/xhci-hub.c
+++ drivers/usb/host/xhci-hub.c
@@ -1343,7 +1343,7 @@ int xhci_hub_control(struct usb_hcd *hcd, u16 typeReq, u16 wValue,
 				}
 				spin_unlock_irqrestore(&xhci->lock, flags);
 				if (!wait_for_completion_timeout(&bus_state->u3exit_done[wIndex],
-								 msecs_to_jiffies(100)))
+								 msecs_to_jiffies(500)))
 					xhci_dbg(xhci, "missing U0 port change event for port %d\n",
 						 wIndex);
 				spin_lock_irqsave(&xhci->lock, flags);
