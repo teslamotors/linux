bc37b05702207a0d9858447f725bd0236a430c05
Johan Hovold <johan+linaro@kernel.org>
2022-08-04 17:09:56 +0200

    usb: dwc3: qcom: fix use-after-free on runtime-PM wakeup
    [ Upstream commit a872ab303d5ddd4c965f9cd868677781a33ce35a ]
    
    The Qualcomm dwc3 runtime-PM implementation checks the xhci
    platform-device pointer in the wakeup-interrupt handler to determine
    whether the controller is in host mode and if so triggers a resume.
    
    After a role switch in OTG mode the xhci platform-device would have been
    freed and the next wakeup from runtime suspend would access the freed
    memory.
    
    Note that role switching is executed from a freezable workqueue, which
    guarantees that the pointer is stable during suspend.
    
    Also note that runtime PM has been broken since commit 2664deb09306
    ("usb: dwc3: qcom: Honor wakeup enabled/disabled state"), which
    incidentally also prevents this issue from being triggered.
    
    Fixes: a4333c3a6ba9 ("usb: dwc3: Add Qualcomm DWC3 glue driver")
    Cc: stable@vger.kernel.org      # 4.18
    Reviewed-by: Matthias Kaehlcke <mka@chromium.org>
    Reviewed-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Signed-off-by: Johan Hovold <johan+linaro@kernel.org>
    Link: https://lore.kernel.org/r/20220804151001.23612-5-johan+linaro@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/usb/dwc3/dwc3-qcom.c
+++ drivers/usb/dwc3/dwc3-qcom.c
@@ -190,6 +190,14 @@ static int dwc3_qcom_register_extcon(struct dwc3_qcom *qcom)
 	return 0;
 }
 
+/* Only usable in contexts where the role can not change. */
+static bool dwc3_qcom_is_host(struct dwc3_qcom *qcom)
+{
+	struct dwc3 *dwc = platform_get_drvdata(qcom->dwc3);
+
+	return dwc->xhci;
+}
+
 static void dwc3_qcom_disable_interrupts(struct dwc3_qcom *qcom)
 {
 	if (qcom->hs_phy_irq) {
@@ -297,7 +305,11 @@ static irqreturn_t qcom_dwc3_resume_irq(int irq, void *data)
 	if (qcom->pm_suspended)
 		return IRQ_HANDLED;
 
-	if (dwc->xhci)
+	/*
+	 * This is safe as role switching is done from a freezable workqueue
+	 * and the wakeup interrupts are disabled as part of resume.
+	 */
+	if (dwc3_qcom_is_host(qcom))
 		pm_runtime_resume(&dwc->xhci->dev);
 
 	return IRQ_HANDLED;
