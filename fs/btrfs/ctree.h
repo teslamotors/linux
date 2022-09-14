bd0fe54891023592a66ce95bd281f84b0324bce9
Josef Bacik <josef@toxicpanda.com>
2022-09-14 19:04:40 -0400

    btrfs: move btrfs_pinned_by_swapfile prototype into volumes.h
    [ Upstream commit c2e79e865b87c2920a3cd39de69c35f2bc758a51 ]
    
    This is defined in volumes.c, move the prototype into volumes.h.
    
    Reviewed-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Reviewed-by: Anand Jain <anand.jain@oracle.com>
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>
    Stable-dep-of: 6bfe3959b0e7 ("btrfs: compare the correct fsid/metadata_uuid in btrfs_validate_super")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/btrfs/ctree.h
+++ fs/btrfs/ctree.h
@@ -482,8 +482,6 @@ struct btrfs_swapfile_pin {
 	bool is_block_group;
 };
 
-bool btrfs_pinned_by_swapfile(struct btrfs_fs_info *fs_info, void *ptr);
-
 enum {
 	BTRFS_FS_BARRIER,
 	BTRFS_FS_CLOSING_START,
