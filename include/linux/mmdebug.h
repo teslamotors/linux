ced17a55a8e7ab251e1349148940a6df43434ed9
Nathan Chancellor <nathan@kernel.org>
2022-11-28 15:53:46 -0700

    mm: Fix '.data.once' orphan section warning
    Portions of upstream commit a4055888629b ("mm/memcg: warning on !memcg
    after readahead page charged") were backported as commit cfe575954ddd
    ("mm: add VM_WARN_ON_ONCE_PAGE() macro"). Unfortunately, the backport
    did not account for the lack of commit 33def8498fdd ("treewide: Convert
    macro and uses of __section(foo) to __section("foo")") in kernels prior
    to 5.10, resulting in the following orphan section warnings on PowerPC
    clang builds with CONFIG_DEBUG_VM=y:
    
      powerpc64le-linux-gnu-ld: warning: orphan section `".data.once"' from `mm/huge_memory.o' being placed in section `".data.once"'
      powerpc64le-linux-gnu-ld: warning: orphan section `".data.once"' from `mm/huge_memory.o' being placed in section `".data.once"'
      powerpc64le-linux-gnu-ld: warning: orphan section `".data.once"' from `mm/huge_memory.o' being placed in section `".data.once"'
    
    This is a difference between how clang and gcc handle macro
    stringification, which was resolved for the kernel by not stringifying
    the argument to the __section() macro. Since that change was deemed not
    suitable for the stable kernels by commit 59f89518f510 ("once: fix
    section mismatch on clang builds"), do that same thing as that change
    and remove the quotes from the argument to __section().
    
    Fixes: cfe575954ddd ("mm: add VM_WARN_ON_ONCE_PAGE() macro")
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>
    Acked-by: Hugh Dickins <hughd@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/mmdebug.h
+++ include/linux/mmdebug.h
@@ -38,7 +38,7 @@ void dump_mm(const struct mm_struct *mm);
 		}							\
 	} while (0)
 #define VM_WARN_ON_ONCE_PAGE(cond, page)	({			\
-	static bool __section(".data.once") __warned;			\
+	static bool __section(.data.once) __warned;			\
 	int __ret_warn_once = !!(cond);					\
 									\
 	if (unlikely(__ret_warn_once && !__warned)) {			\
