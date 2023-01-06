4923160393b06a34759a11b17930d71e06f396f2
Hao Sun <sunhao.th@gmail.com>
2023-01-06 16:48:38 +0800

    bpf: Skip task with pid=1 in send_signal_common()
    [ Upstream commit a3d81bc1eaef48e34dd0b9b48eefed9e02a06451 ]
    
    The following kernel panic can be triggered when a task with pid=1 attaches
    a prog that attempts to send killing signal to itself, also see [1] for more
    details:
    
      Kernel panic - not syncing: Attempted to kill init! exitcode=0x0000000b
      CPU: 3 PID: 1 Comm: systemd Not tainted 6.1.0-09652-g59fe41b5255f #148
      Call Trace:
      <TASK>
      __dump_stack lib/dump_stack.c:88 [inline]
      dump_stack_lvl+0x100/0x178 lib/dump_stack.c:106
      panic+0x2c4/0x60f kernel/panic.c:275
      do_exit.cold+0x63/0xe4 kernel/exit.c:789
      do_group_exit+0xd4/0x2a0 kernel/exit.c:950
      get_signal+0x2460/0x2600 kernel/signal.c:2858
      arch_do_signal_or_restart+0x78/0x5d0 arch/x86/kernel/signal.c:306
      exit_to_user_mode_loop kernel/entry/common.c:168 [inline]
      exit_to_user_mode_prepare+0x15f/0x250 kernel/entry/common.c:203
      __syscall_exit_to_user_mode_work kernel/entry/common.c:285 [inline]
      syscall_exit_to_user_mode+0x1d/0x50 kernel/entry/common.c:296
      do_syscall_64+0x44/0xb0 arch/x86/entry/common.c:86
      entry_SYSCALL_64_after_hwframe+0x63/0xcd
    
    So skip task with pid=1 in bpf_send_signal_common() to avoid the panic.
    
      [1] https://lore.kernel.org/bpf/20221222043507.33037-1-sunhao.th@gmail.com
    
    Signed-off-by: Hao Sun <sunhao.th@gmail.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Stanislav Fomichev <sdf@google.com>
    Link: https://lore.kernel.org/bpf/20230106084838.12690-1-sunhao.th@gmail.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -649,6 +649,9 @@ BPF_CALL_1(bpf_send_signal, u32, sig)
 		return -EPERM;
 	if (unlikely(!nmi_uaccess_okay()))
 		return -EPERM;
+	/* Task should not be pid=1 to avoid kernel panic. */
+	if (unlikely(is_global_init(current)))
+		return -EPERM;
 
 	if (irqs_disabled()) {
 		/* Do an early check on signal validity. Otherwise,
